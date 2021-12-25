880ab9ced14d0ba1de31de87e4aa96efd793d2ba
shashank524 <saishashank524@gmail.com>
2021-12-25 22:07:20 +0530

    web scraper update

--- news_web_scraper.py
+++ news_web_scraper.py
@@ -1,5 +1,6 @@
 import datetime
 import pandas as pd
+import time
  
 from datetime import date
 from datetime import timedelta
@@ -8,7 +9,7 @@ start_date = date(2021, 1, 1)
 todays_date = date.today()
 
 no_days = int((todays_date-start_date).days)
-
+print(no_days, " days")
 index = pd.date_range(start_date, periods=no_days, freq='D')
  
 columns = ['']
@@ -25,6 +26,7 @@ end_index=start_index+no_days
 headlies = []
 
 for i in range(start_index, end_index):
+    print("day: ", i-start_index)
     path = 'https://economictimes.indiatimes.com/archivelist/year-2021,month-1,starttime-{}.cms'.format(str(i))
     html_text = requests.get(path).text
 
@@ -32,9 +34,10 @@ for i in range(start_index, end_index):
 
     container = soup.find('li')
     headlies.append(container.text)
-
+    # time.sleep(0.5)
 # df.insert(1, "headlines_text", headlies)
 
 df = pd.DataFrame({"publish_date":index, "headline_text":headlies})
 print(df.tail())
 df.to_csv('new_headlines.csv')
+
